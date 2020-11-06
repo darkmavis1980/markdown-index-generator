8e140ba898f4aefd08efac3bf6b2826992a6a960
Alessio Michelini <amichelini@hubspot.com>
2020-11-06 20:13:05 +0000

    Improved logic to find for headings in a document Added function replaceBlock with relative unit tests Added missing jsdoc comments

--- src/lib/tags.ts
+++ src/lib/tags.ts
@@ -1,3 +1,11 @@
+/**
+ * It replace the content of a given tag from a source string
+ *
+ * @param {string} source The text to replace
+ * @param {string} tag The tag to find
+ * @param {string} content The content to replace in between the tags
+ * @returns {string} The parsed string
+ */
 export const replaceTag = (source: string, tag: string, content: string): string => {
   const start = `<!-- ${tag}-start -->`
   const end = `<!-- ${tag}-end -->`
@@ -9,4 +17,16 @@ export const replaceTag = (source: string, tag: string, content: string): string
   const preTagContent = source.slice(0, tagStartPosition + start.length)
   const postTagContent = source.slice(tagEndPosition)
   return `${preTagContent}\n${content}\n${postTagContent}\n`
+}
+
+/**
+ * Takes a string as an input and remove the content in between the blocks
+ *
+ * @param {string} source The source text to replace
+ * @param {string} block The block of repeated code to encapsulate content
+ * @returns {string} The replaced string
+ */
+export const replaceBlock = (source: string, block: string): string => {
+  const regex = new RegExp(`${block}(.+?)${block}`, 'gms')
+  return source.replace(regex, '')
 }
--- test/lib/tags.test.ts
+++ test/lib/tags.test.ts
@@ -1,26 +1,64 @@
 import {expect} from '@oclif/test'
-import {replaceTag} from '../../src/lib/tags'
+import {replaceTag, replaceBlock} from '../../src/lib/tags'
 
-describe('replacetag (Function)', () => {
-  it('should replace the content', () => {
-    const mockContent = `# Some Title
-    <!-- test-start -->
-    <!-- test-end -->
-    Some test`
-    const result = replaceTag(mockContent, 'test', 'hello')
-    expect(result).to.contain('hello')
+describe('tags.ts', () => {
+  describe('replacetag (Function)', () => {
+    it('should replace the content', () => {
+      const mockContent = `# Some Title
+      <!-- test-start -->
+      <!-- test-end -->
+      Some test`
+      const result = replaceTag(mockContent, 'test', 'hello')
+      expect(result).to.contain('hello')
+    })
+
+    it('should throw an error if a tag is missing', () => {
+      const mockContent = `# Some Title
+      <!-- test-start -->
+      Some test`
+
+      expect(() => replaceTag(mockContent, 'test', 'hello')).to.throw()
+
+      const mockContent2 = `# Some Title
+      Some test`
+
+      expect(() => replaceTag(mockContent2, 'test', 'hello')).to.throw()
+    })
   })
 
-  it('should throw an error if a tag is missing', () => {
-    const mockContent = `# Some Title
-    <!-- test-start -->
-    Some test`
+  describe('replaceBlock (Function)', () => {
+    it('should remove the block from the content', () => {
+      const mockContent = `# Some Title
+      \`\`\`
+      Find Me
+      \`\`\`
+      Some test`
+      const result = replaceBlock(mockContent, '```')
+      expect(result).to.not.contain('Find Me')
+    })
 
-    expect(() => replaceTag(mockContent, 'test', 'hello')).to.throw()
+    it('should not remove everything else', () => {
+      const mockContent = `# Some Title
+      \`\`\`
+      Find Me
+      \`\`\`
+      Some test`
+      const result = replaceBlock(mockContent, '```')
+      expect(result).to.contain('Some test')
+    })
 
-    const mockContent2 = `# Some Title
-    Some test`
-
-    expect(() => replaceTag(mockContent2, 'test', 'hello')).to.throw()
+    it('should remove every instance of the block from the content', () => {
+      const mockContent = `# Some Title
+      \`\`\`
+      Find Me
+      \`\`\`
+      ---
+      \`\`\`
+      Find Me
+      \`\`\`
+      Some test`
+      const result = replaceBlock(mockContent, '```')
+      expect(result).to.not.contain('Find Me')
+    })
   })
 })
