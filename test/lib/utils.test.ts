3d72079650aa7037bc3df8a5ac9d8d5b842005ee
Alessio Michelini <amichelini@hubspot.com>
2022-12-02 14:51:55 +0000

    Removed mocha in favour of Jest Updated all tests to run with Jest

--- test/lib/utils.test.ts
+++ test/lib/utils.test.ts
@@ -1,21 +1,20 @@
-import {expect} from '@oclif/test'
 import {stringToPermalink} from '../../src/lib/utils'
 
 describe('Utils', () => {
   describe('stringToPermalink', () => {
     it('should return the url formatted string', () => {
-      expect(stringToPermalink('Hello world')).to.equal('hello-world')
+      expect(stringToPermalink('Hello world')).toEqual('hello-world');
     })
 
     it('should escape punctuation characters', () => {
-      expect(stringToPermalink('Hello, world')).to.equal('hello-world')
-      expect(stringToPermalink('Hello. World!')).to.equal('hello-world')
+      expect(stringToPermalink('Hello, world')).toEqual('hello-world');
+      expect(stringToPermalink('Hello. World!')).toEqual('hello-world');
     })
 
     it('should escape special characters', () => {
-      expect(stringToPermalink('Hello, @@--`world')).to.equal('hello-world')
-      expect(stringToPermalink('Hello /world!****')).to.equal('hello-world')
-      expect(stringToPermalink('---Hello---world---')).to.equal('helloworld')
+      expect(stringToPermalink('Hello, @@--`world')).toEqual('hello-world');
+      expect(stringToPermalink('Hello /world!****')).toEqual('hello-world');
+      expect(stringToPermalink('---Hello---world---')).toEqual('helloworld');
     })
   })
 })
