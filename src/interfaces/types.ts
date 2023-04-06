86d0ae67f83d11986ef4d5ccc909499ef1d0fb0a
Alessio Michelini <amichelini@hubspot.com>
2023-04-06 10:05:46 +0100

    [BREAKING] Changed the default behaviour to update the file Upgraded to TS 5 Updated dependencies Updated tests to match new default behaviour Code cleanups

--- src/interfaces/types.ts
+++ src/interfaces/types.ts
@@ -1,8 +1,8 @@
 export type HeadingsRange = 2|3|4|5;
 
 export interface Flags {
-  depth: number;
-  replace: boolean;
+  depth?: number;
+  dryRun?: boolean;
   title?: string;
   output?: string;
 }
