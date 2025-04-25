62b29159c495c71f134681b7c668ca701ed3de8b
Alessio Michelini <amichelini@hubspot.com>
2025-04-25 11:45:20 +0100

    style: format whitespace handling in stringToPermalink function for consistency

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,7 +7,7 @@
 export function stringToPermalink(string: string): string {
   return string
     .trim()
-    .replace(/\s+/g, ' ')    // Replace multiple whitespaces with a single space
+    .replace(/\s+/g, ' ') // Replace multiple whitespaces with a single space
     .replace(/(-)+\1+/g, '')
     .replace(/\s/g, '-')
     .replace(/[^\d\w_-]/g, '')
