25fff8222919d4c665ebe36aca9b3d353b5d6424
Alessio Michelini <amichelini@hubspot.com>
2025-08-12 16:44:31 +0100

    test: remove unused replaceBlock import and update test cases for consistency

--- test/lib/tags.test.ts
+++ test/lib/tags.test.ts
@@ -3,10 +3,7 @@ import { replaceTag, replaceBlock, findFirstParagraph } from '../../src/lib/tags
 describe('tags.ts', () => {
   describe('findFirstParagraph (Function)', () => {
     it('should add the index tag', () => {
-      const mockContent = `# Some title\n
-A paragraph\n
-## Subtitle\n
-Some text\n`;
+      const mockContent = `# Some title\n\nA paragraph\n## Subtitle\nSome text\n`;
 
       const result = findFirstParagraph(mockContent);
       expect(result).toContain('<!-- index-start -->');
@@ -14,19 +11,7 @@ Some text\n`;
     });
 
     it('should not pull content from below H2 headings', () => {
-      const mockContent = `# Test
-
-## A
-
-Some H2 text that should not be pulled to heading
-
-### B - not pulled
-
-Some H3 text not pulled to heading
-
-## C
-
-### D - H3 should not be pulled because parent H2 has no text`;
+      const mockContent = `# Test\n\n## A\n\nSome H2 text that should not be pulled to heading\n\n### B - not pulled\n\nSome H3 text not pulled to heading\n\n## C\n\n### D - H3 should not be pulled because parent H2 has no text`;
 
       const result = findFirstParagraph(mockContent);
       expect(result).toContain('## A\n\nSome H2 text that should not be pulled to heading');
@@ -38,28 +23,21 @@ Some H3 text not pulled to heading
 
   describe('replacetag (Function)', () => {
     it('should replace the content', () => {
-      const mockContent = `# Some Title
-      <!-- test-start -->
-      <!-- test-end -->
-      Some test`;
+      const mockContent = `# Some Title\n      <!-- test-start -->\n      <!-- test-end -->\n      Some test`;
       const result = replaceTag(mockContent, 'test', 'hello');
       expect(result).toContain('hello');
     });
 
     it('should throw an error if a tag is missing', () => {
-      const mockContent = `# Some Title
-      <!-- test-start -->
-      Some test`;
+      const mockContent = `# Some Title\n      <!-- test-start -->\n      Some test`;
 
       expect(() => replaceTag(mockContent, 'test', 'hello')).toThrow();
 
-      const mockContent2 = `# Some Title
-      Some test`;
+      const mockContent2 = `# Some Title\n      Some test`;
 
       expect(() => replaceTag(mockContent2, 'test', 'hello')).toThrow();
     });
   });
-
   describe('replaceBlock (Function)', () => {
     it('should remove the block from the content', () => {
       const mockContent = `# Some Title
